e6d7914d99df641e82f23039aaefdbbc86a46ec9
Yasser-Shehab <yassershehab9@gmail.com>
2023-01-18 14:59:25 +0200

    Added Tours data

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from './assets/images/tour-1.jpeg';
+import tour2 from './assets/images/tour-2.jpeg';
+import tour3 from './assets/images/tour-3.jpeg';
+import tour4 from './assets/images/tour-4.jpeg';
+
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
@@ -29,5 +34,48 @@ export const services = [
     icon: 'fas fa-socks fa-fw',
     title: 'amazing comfort',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    location: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 4,
+    location: 4200,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'US',
+    duration: 6,
+    location: 2100,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    info: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'Egypt',
+    duration: 6,
+    location: 1500,
   },
 ];
